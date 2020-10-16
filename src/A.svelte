eb42b02bbe8aab7f2bd01c70322e439555ed6894
Holger H <holgheiss@gmail.com>
2020-10-16 14:31:24 +0200

    cursor:pointer simulation for links and language

--- src/A.svelte
+++ src/A.svelte
@@ -1,11 +1,15 @@
 <script>
+  import { hover } from './stores.js';
+
   export let href, text, newtab
+
+  const toggleHovering = toggle => hover.update(()=>toggle);
 
   $: target = newtab ? "target=\"blank\"" : null
 
 </script>
 
-<a href="{href}" {target} title={text}>{text}</a>
+<a href="{href}" {target} title={text} on:mouseenter={()=>toggleHovering(true)} on:mouseleave={()=>toggleHovering(false)} >{text}</a>
 
 <style>
   a {
--- src/Language.svelte
+++ src/Language.svelte
@@ -1,17 +1,23 @@
 <script>
+  import { hover } from './stores.js';
+
   export let visible, setLanguage, language
+
+  const toggleHovering = toggle => hover.update(()=>toggle);
+
 </script>
 
 <nav class:visible={visible}>
   <ul>
-    <li on:click={ e => setLanguage("en") } class:active={language === "en"}>
+    <li on:click={ e => setLanguage("en") } class:active={language === "en"} on:mouseenter={()=>toggleHovering(language !== "en" && true)} on:mouseleave={()=>toggleHovering(false)}>
       <abbr title="English">en</abbr>
     </li>
     <li class="separator">  / </li>
-    <li on:click={ e => setLanguage("de") } class:active={language === "de"}>
+    <li on:click={ e => setLanguage("de") } class:active={language === "de"} on:mouseenter={()=>toggleHovering(language !== "de" && true)} on:mouseleave={()=>toggleHovering(false)}>
       <abbr title="Deutsch">de</abbr>
     </li>    
   </ul>
+  
 </nav>
 
 <style>
@@ -25,6 +31,7 @@
     z-index: 100;
     font-size: 24px;
     line-height: 25px;
+    pointer-events: none;
   }
   @media (max-width: 500px) {
     nav {
@@ -48,6 +55,7 @@
   }
   li:not(.active):not(.separator) {
     cursor: pointer;
+    pointer-events: all;
   }
   nav:not(.visible) {
     opacity: 0;
