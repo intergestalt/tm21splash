cbaf63f8b1237da3e3727d224633aae2166914ec
tm2122 <73097156+tm2122@users.noreply.github.com>
2020-10-19 15:42:47 +0200

    Update FooterDE.svelte

--- src/content/FooterDE.svelte
+++ src/content/FooterDE.svelte
@@ -18,10 +18,10 @@
 
 <p>
   The festival has been funded as a cultural institution of excellence by
-  Kulturstiftung des Bundes (German Federal Cultural Foundation) since 2004. 
+  Kulturstiftung des Bundes (German Federal Cultural Foundation) since 2004.<br> 
   Additional funding by Senatsverwaltung
   für Kultur Berlin, European Union’s Horizon 2020, Bundeszentrale für
-  politische Bildung and Medienboard Berlin Brandenburg. 
+  politische Bildung and Medienboard Berlin Brandenburg. <br> 
   <A text="See all our supporters" href="https://transmediale.de/de/partners" />.
 </p>
 
