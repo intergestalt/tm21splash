ed27ce290dd52483a0f8aca3c9803ebd195c549a
tm2122 <73097156+tm2122@users.noreply.github.com>
2021-01-17 13:57:57 +0100

    Update TextEN.svelte

--- src/content/TextEN.svelte
+++ src/content/TextEN.svelte
@@ -15,9 +15,81 @@
   */
 
 </script>
-
 <p>
-  The dilemma for refusal has never felt quite so urgent. Easily misunderstood
+  The upcoming edition of transmediale takes place over the course of a year 
+  and presents a collection of exhibitions, screenings, and discourse 
+  programmes, which, along with residencies, workshops, and commissions, will 
+  explore the material, technological, and cultural aspects of refusal. 
+  Unfolding via online and in-person events, this new expanded structure, 
+  formulated in response to COVID-19, will create a year-long engagement 
+  with the festival theme.
+</p>
+<p>
+  The festival begins on 28 January with the launch of our new digital platform,
+  the <i>Almanac for Refusal</i>. The exhibition <i>Rendering Refusal</i> will 
+  take place across two venues in Kunstraum Kreuzberg/Bethanien and Betonhalle, 
+  Silent Green Kulturquartier. The film programme <i>remote.response.request.</i>
+  presents a monthly series of film screenings and artist exchanges installed 
+  at transmediale studio at Silent Green Kulturquartier. 
+</p>
+<p>
+  Public access to the exhibition and film programme will be available as soon 
+  as government regulations around the pandemic allow. We will facilitate 
+  additional access to the exhibition <i>Rendering Refusal</i> through a remote 
+  visitor programme and extensive documentation.
+</p>
+<h3>Almanac for Refusal</h3>
+<p>
+  As part of its year-long programme transmediale 2021–22 launches a new 
+  digital platform on 28 January. Our <i>Almanac for Refusal</i> brings 
+  together a collection of browser-based artworks; films, photo-essays, texts, 
+  graphs, images, podcasts, sound experiments, and more. Inspired by almanac 
+  traditions, the collection will update at the end of each lunar cycle and act
+  as a guide to the festival. Throughout the year, the <i>Almanac for Refusal</i> 
+  will include contributions and artworks from artists, designers, researchers, 
+  and activists whose practices map out the ways refusal can unfold and how it 
+  circumvents the status quo.
+</p>
+<h3>Exhibition</h3>
+<h3>Rendering Refusal</h3>
+<p>
+  Exploring what can emerge when hope and refusal work together, <i>Rendering Refusal</i> 
+  presents fourteen artists whose works enact imaginative, speculative, and 
+  poetic strategies of non-compliance. Taking place across two venues in 
+  Kunstraum Kreuzberg/Bethanien and Betonhalle, Silent Green Kulturquartier, 
+  the exhibition runs until 28 March. Due to the current COVID-19 pandemic, 
+  public access to the exhibition will be available as soon as government 
+  regulations around the pandemic allow.
+</p>
+<p>
+  We will facilitate access to the exhibition both through extensive documentation
+  and remote visits. Visitors can connect via video call with an exhibition 
+  guide at the venue and experience the exhibition with them. Through these 
+  remote visits, we wish to provide a unique viewing experience, enabled by 
+  technology, that widen accessibility to both venues. Remote visits can be 
+  booked for both venues on our website from February.
+</p>
+<h3>Film Programme</h3>
+<h3>remote.response.request.</h3>
+<p>
+  Conceived in response to film’s current state of suspension during the 
+  pandemic, <i>remote.response.request.</i> maps new approaches to ‘add to the 
+  image’ through three-month-long installations at the transmediale studio. 
+  Forming new relations between viewers and the moving image, these 
+  installations bring together filmmakers, academics, and performers, in a 
+  series of commissioned works produced at a distance. The first series of 
+  <i>remote.response.request.</i>, invokes the evocative concept of the radical 
+  imagination through work that ask us not only to envision but to live, 
+  different ways of being.
+</p>
+<p>
+  Due to the current COVID-19 pandemic, public access to the film programme 
+  will be available as soon as government regulations allow. 
+</p>
+<h3>Theme</h3>
+<h3>for refusal</h3>
+<p>
+  The dilemma <i>for refusal</i> has never felt quite so urgent. Easily misunderstood
   as a gesture of inaction and passivity where nothing happens, a refusal
   instead can be an insistence on an alternative or a demand for reform. From
   boycotts to strikes to collective or individual withdrawals, the long and
@@ -25,6 +97,7 @@
   say more than a mere no. As an act that often carries with it both risk and
   promise, a refusal opens up possibilities for worlds that can and should be.
 </p>
+
 <p>
   The task of refusing is not without its difficulties – it is made up of
   compromise and friction, and demands for acting and imagining in ways that lie
@@ -46,22 +119,35 @@
   its potential to form new socio-political realities grounded in care, hope,
   and desire.
 </p>
+<h3>transmediale x CTM collaboration</h3>
 <p>
-  Taking place over the course of a year, the upcoming edition of transmediale
-  will present a collection of exhibitions, screenings, and discourse
-  programmes, which, along with residencies, workshops, and commissions, will
-  explore the material, technological, and cultural aspects of refusal.
-  Unfolding via online and in-person events, this new expanded structure,
-  formulated in response to COVID-19, will create a year-long engagement with
-  the festival theme.
+  <i>rehearsal letter</i> is a recorded studio session for transmediale x CTM 
+  2021, produced as part of the soundtrack for artist Frances Scott’s 
+  forthcoming work <i>Wendy</i>, a film fan-letter to composer and musician 
+  Wendy Carlos. In 2016, musician and instrument designer Tom Richards built 
+  the <i>Mini Oramics machine</i>, based on an original 1976 design by 
+  composer and inventor Daphne Oram. This musical interface allows the
+  composer to draw graphic scores on overhead projector rolls, which are then 
+  read directly by the machine, creating ‘drawn sound synthesis’. Here, 
+  drawn-sound is used as a method of re-interpreting pages from Carlos’ 
+  original scores from <i>Timesteps and Theme Tune for A Clockwork Orange (Beethovania)</i> 
+  (1971).
 </p>
-
 <p>
-  With an exhibition across two venues in Berlin, and an extended film programme 
-  at transmediale studio, our year-long festival begins on 23 January 2021. 
-  Challenging systems of control and exerting new socio-political pressures, 
-  the first part of our programme brings together a collection of artistic positions 
-  that offer possibilities and hope through tactics for refusal. 
+  <i>rehearsal letter</i> will be accessible online in the <i>Almanac for Refusal</i> from 28 January.
+</p>
+<h3>VORSPIEL</h3>
+<p>
+  Starting from 15 January, the 10th-anniversary edition of Vorspiel will take 
+  place online and in-person. Presented by transmediale and CTM, the 
+  pre-festival programme will once more offer space for digital art and culture,
+  as well as experimental music and sound art. Curated by the 35 participating
+  venues, the programme aims to facilitate critical discourse on art, technology,
+  politics, and identity through ongoing exchange and reflection.
+</p>
+<p>
+Further information on participating venues and events will be listed on the 
+  <A href="https://vorspiel.berlin" newtab text=" Vorspiel website" />.
 </p>
  
 
