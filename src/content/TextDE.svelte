30fc345ea1917d3e438c923025af748726a3b07d
tm2122 <73097156+tm2122@users.noreply.github.com>
2021-01-17 14:34:32 +0100

    Update TextDE.svelte

--- src/content/TextDE.svelte
+++ src/content/TextDE.svelte
@@ -15,7 +15,91 @@
   */
 
 </script>
-
+<p>
+  Das Einjahres-Programm der transmediale 2021-22 präsentiert  Ausstellungen, 
+  Screenings und Diskursprogramm sowie Residencies,  Workshops und 
+  Auftragsarbeiten, um sich nachhaltig mit den materiellen, technologischen und 
+  kulturellen Aspekten von Verweigerung  auseinanderzusetzen. Die neue, 
+  erweiterte Struktur, die sich in online- und in-person-Veranstaltungen 
+  entfaltet, ist eine direkte Reaktion auf COVID-19 und soll eine ganzjährige 
+  Auseinandersetzung mit dem Festivalthema ermöglichen.
+</p>
+<p>
+ Das Einjahres-Festival beginnt am 28. Januar mit der Veröffentlichung unserer 
+  digitalen Plattform <i>Almanac for Refusal</i>. Die Ausstellung 
+  <i>Rendering Refusal</i> findet in den zwei Venues – Kunstraum 
+  Kreuzberg/Bethanien und Betonhalle, Silent Green Kulturquartier – statt. 
+  Das Filmprogramm <i>remote.response.request.</i>, installiert im transmediale 
+  studio im Silent Green Kulturquartier, präsentiert eine monatlich wechselnde 
+  Reihe von Film Screenings und Diskursprogramm.
+ </p>
+<p>
+  <i>Rendering Refusal</i> und <i>remote.response.request.</i>werden öffentlich 
+  zugänglich sein, sobald die offiziellen Pandemie-Regelungen es erlauben. 
+  Remote Visits und eine umfangreiche Dokumentation machen die Ausstellung ab 
+  Februar digital erfahrbar. 
+</p>
+<h3>Almanac for Refusal</h3>
+<p>
+  Zu Beginn des Einjahres-Festivals veröffentlichen wir die digitale Plattform 
+  <i>Almanac for Refusal</i>. Über den Zeitraum eines Jahres werden hier eine 
+  Reihe browserbasierter Kunstwerke wie Filme, Video-Essays, Texte, Grafiken, 
+  Bilder, Podcasts und Soundexperimente zu sehen sein. Inspiriert von den 
+  Traditionen des Almanachs, wird die Sammlung zum Ende jedes Mondzyklus um 
+  neue Inhalte ergänzt. In diesen Beiträgen und Dokumentationen kommen Arbeiten 
+  von Künstler*innen, Designer*innen, Wissenschaftler*innen und Aktivist*innen 
+  zusammen, die in ihrer Praxis aufzeigen, wie sich Verweigerung entfalten und 
+  den Status quo umgehen kann. 
+</p>
+<h3>Ausstellung<br />
+  Rendering Refusal</h3>
+<p>
+  Die Ausstellung <i>Rendering Refusal</i> präsentiert vierzehn Künstler*innen, die 
+  in ihren Arbeiten fiktive, spekulative und poetische Strategien der 
+  Verweigerung umsetzen und erforschen, was entstehen kann, wenn Hoffnung und 
+  Verweigerung zusammenarbeiten.
+</p>
+<p>
+  Die Ausstellung findet in den zwei Berliner Venues – Kunstraum 
+  Kreuzberg/Bethanien und Betonhalle, Silent Green Kulturquartier – statt und 
+  läuft bis zum 28. März. 
+</p>  
+<p>
+  <i>Rendering Refusal</i> wird öffentlich zugänglich sein, sobald die offiziellen 
+  Pandemie-Regelungen es erlauben. Remote Visits und eine umfangreiche 
+  Dokumentation machen die Ausstellung ab Februar digital erfahrbar. 
+</p>  
+<p>
+  Für die Remote Visits werden Besuchende per Videoanruf mit einer*m Guide 
+  vor Ort in einer der Venues verbunden und erleben die Ausstellung so 
+  gemeinsam und in Interaktion. Durch die Remote Visits wollen wir eine 
+  besondere Ausstellungserfahrung bieten, welche die Zugänglichkeit zu beiden 
+  Ausstellungsvenues ermöglicht.
+</p> 
+<p>
+  Remote visits für beide Venues können ab Februar auf unserer Webseite 
+  gebucht werden.
+</p> 
+<h3>Film Programm<br />
+  remote.response.request.</h3>
+<p>
+  <i>remote.response.request.</i> versteht sich als der Versuch einer 
+  Entgegnung auf den durch die Pandemie ausgelösten Schwebezustand des Films. 
+  In einer dreimonatigen Installation im transmediale studio präsentiert das 
+  Film Programm neue filmische Visionen und bringt dabei die Arbeiten von 
+  Filmemacher*innen, Akademiker*innen und Performer*innen zusammen.
+</p>  
+<p>
+  Die erste Serie von <i>remote.response.request.</i> beschwört das Konzept 
+  der radikalen Imagination durch Arbeiten, die dazu auffordern, uns andere 
+  Lebensweisen nicht nur vorzustellen, sondern auch zu leben.
+</p>
+<p>
+  Aufgrund der aktuellen COVID-19-Pandemie wird <i>remote.response.request.</i> 
+  öffentlich zugänglich sein, sobald die offiziellen Regelungen es erlauben.
+</p>
+<h3>Thema<br />
+  for refusal</h3>
 <p>
   Das Dilemma der Verweigerung hat zunehmend an Dringlichkeit gewonnen. Leicht
   missverstanden als Geste der Untätigkeit und Passivität versinnbildlicht
@@ -52,22 +136,36 @@
   sozio-politische Realitäten zu formen, die auf Fürsorge, Hoffnung und Begehren
   beruhen.
 </p>
+<h3>transmediale x CTM 2021</h3>
 <p>
-  Durch eine erweiterte Struktur wird sich die kommende Ausgabe der transmediale
-  in einem fortlaufenden Programm über ein Jahr entwickeln. Neben
-  Künstler*innenresidenzen, Workshops und Auftragsarbeiten präsentiert das
-  Einjahres-Festival Ausstellungen, Screenings sowie ein Diskursprogramm, um
-  sich nachhaltig mit den materiellen, technologischen und kulturellen Aspekten
-  von Verweigerung auseinanderzusetzen. Diese neue, auch als Antwort auf
-  COVID-19 formulierte Struktur, findet in Form von verschiedenen
-  Veranstaltungsformaten in Berlin und online statt. 
+  <i>rehearsal letter</i> ist eine aufgezeichnete Studiosession der Künstlerin 
+  Frances Scott, die zur Entwicklung des Soundtracks ihres nächsten 
+  Filmprojektes <i>Wendy</i> – ein filmischer Fan-Brief an die Komponistin und 
+  Musikerin Wendy Carlos – entstand. 2016 baute der Musiker und Instrumentendesigner 
+  Tom Richards die <i>Mini Oramics</i> Maschine, die auf einen Originalentwurf 
+  der Komponistin Daphne Oram aus dem Jahr 1976 zurückgeht. Diese musikalische 
+  Schnittstelle  ermöglicht es dem Komponisten, grafische Partituren auf 
+  Overhead-Projektorrollen zu zeichnen, die dann direkt von der Maschine gelesen 
+  werden und eine ‘gezeichnete Klangsynthese’ erzeugen. In Scotts Videoarbeit 
+  wird diese Methode verwendet, um Seiten aus Carlos' Originalpartituren von 
+  <i>Timesteps und Theme Tune for A Clockwork Orange (Beethovania)</i> (1971) 
+  neu zu interpretieren. 
+</p>  
+<p>
+  <i>rehearsal letter</i> ist ab dem 28. Januar im <i>Almanac for Refusal</i> online.
+</p>   
+<h3>VORSPIEL</h3>
+<p>
+ Ab dem 15. Januar präsentieren transmediale & CTM Festival die 10. 
+  Jubiläumsausgabe des Pre-Festival Programmes Vorspiel. Mit einer Mischung 
+  aus online und in-person Veranstaltungen bieten Projekträume, Galerien und 
+  Kulturinstitutionen erneut Raum für digitale Kunst und Kultur sowie 
+  experimentelle Musik und Sound Art. Kuratiert von den teilnehmenden Venues, 
+  soll <i>Vorspiel</i> den kritischen Diskurs über Kunst, Technologie, Politik 
+  und Identität sowie Austausch und Reflexion ermöglichen.
 </p>
-<p>Mit einer Ausstellung in zwei Venues in Berlin und einem Filmprogramm im 
-  transmediale Studio beginnen wir am 23. Januar 2021 unser Einjahres-Festival.  
-  Der erste Teil unseres Jahresprogramms bringt künstlerische Positionen zusammen, 
-  die Kontrollsysteme infrage stellen und alternative Formen gesellschaftlichen Drucks 
-  ausüben, um durch Taktiken der Verweigerung Raum für Möglichkeiten und Hoffnung 
-  zu schaffen.
+<p>
+  Weitere Informationen zu den teilnehmenden Venues und Veranstaltungen findet 
+  ihr auf der <A href="https://vorspiel.berlin" newtab text="Vorspiel Webseite" />.
 </p>
-
 
